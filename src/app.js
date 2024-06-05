452dbeba9745924d9d021216a60e8e6f45f1e8d2
S-Sotelo <168486323+S-Sotelo@users.noreply.github.com>
2024-06-05 15:29:40 +0000

    excuse-generator

--- src/app.js
+++ src/app.js
@@ -5,7 +5,49 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+let who = [
+  "El amigo de mi perro",
+  "Mi hermano pequeño",
+  "El gato Roberto",
+  "El Spy"
+];
+let action = [
+  "se comió",
+  "vendió",
+  "bailó con mi abuela",
+  "rompió con un sartén"
+];
+let what = [
+  "mi proyecto de 4geeks",
+  "el computador de mi tatarabuela",
+  "mi auto nuevo"
+];
+let when = [
+  "ayer",
+  "mientras jugaba play con los chicos de mi curso de 4geeks",
+  "cuando comía cereales en espacio exterior",
+  "el lunes 20 de junio del año 2000",
+  "cuando estaba dando clases en un avión"
+];
+
+function getRandomNumber(a, b) {
+  return Math.floor(Math.random() * b - a + a);
+}
+
+function getRandom(anyArray) {
+  let max = who.length - 1;
+  let min = 0;
+  let random = getRandomNumber(min, max);
+  return anyArray[random];
+}
+
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector("#excuse").innerHTML =
+    getRandom(who) +
+    " " +
+    getRandom(action) +
+    " " +
+    getRandom(what) +
+    " " +
+    getRandom(when);
 };
